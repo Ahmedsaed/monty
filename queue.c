ef29c3ef0e6c4a76208b1d66934cb8778bbb410f
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-05-26 17:51:04 +0300

    update code style and documentation

--- queue.c
+++ queue.c
@@ -1,81 +1,107 @@
 #include "monty.h"
 
-// Queue push_back operation
+/* stack and queue functions */
+void init_stack(stack_t *stack);
+void stack_push_back(stack_t *stack, int value);
+int stack_pop_back(stack_t *stack);
+int stack_front(stack_t *stack);
+void queue_push_back(stack_t *stack, int value);
+void queue_push_front(stack_t *stack, int value);
+int queue_pop_back(stack_t *stack);
+int queue_pop_front(stack_t *stack);
+int queue_front(stack_t *stack);
+int queue_back(stack_t *stack);
+
+/**
+ * queue_push_back - adds a new node at the end of a stack_t list
+ *
+ * @stack: pointer to the stack
+ * @value: value to be added to the stack
+ *
+ * Return: the address of the new element, or NULL if it failed
+*/
 void queue_push_back(stack_t *stack, int value)
 {
-    stack_push_back(stack, value);
+	stack_push_back(stack, value);
 }
 
-// Queue push_front operation
+/**
+ * queue_push_front - adds a new node at the beginning of a stack_t list
+ *
+ * @stack: pointer to the stack
+ * @value: value to be added to the stack
+*/
 void queue_push_front(stack_t *stack, int value)
 {
-    stack_t *new_node = (stack_t *)malloc(sizeof(stack_t));
-    new_node->n = value;
-    new_node->prev = NULL;
+	stack_t *new_node = (stack_t *)malloc(sizeof(stack_t));
 
-    if (stack->n == 0)
-    {
-        new_node->next = NULL;
-        stack->next = new_node;
-    }
-    else
-    {
-        new_node->next = stack->next;
-        stack->next->prev = new_node;
-        stack->next = new_node;
-    }
+	new_node->n = value;
+	new_node->prev = NULL;
 
-    stack->n++;
+	if (stack->n == 0)
+	{
+		new_node->next = NULL;
+		stack->next = new_node;
+	}
+	else
+	{
+		new_node->next = stack->next;
+		stack->next->prev = new_node;
+		stack->next = new_node;
+	}
+
+	stack->n++;
 }
 
-// Queue pop_back operation
+/**
+ * queue_pop_back - removes the last element of a stack_t list
+ *
+ * @stack: pointer to the stack
+ *
+ * Return: the value of the removed element
+*/
 int queue_pop_back(stack_t *stack)
 {
-    return stack_pop_back(stack);
+	return (stack_pop_back(stack));
 }
 
-// Queue pop_front operation
+/**
+ * queue_pop_front - removes the first element of a stack_t list
+ *
+ * @stack: pointer to the stack
+ *
+ * Return: the value of the removed element
+*/
 int queue_pop_front(stack_t *stack)
 {
-    if (stack->n == 0)
-    {
-        printf("Queue is empty.\n");
-        return -1;
-    }
+	int value;
+	stack_t *temp;
 
-    int value = stack->next->n;
-    stack_t *temp = stack->next;
+	if (stack->n == 0)
+		return (-1);
 
-    if (stack->next->prev != NULL)
-        stack->next->prev->next = NULL;
+	value = stack->next->n;
+	temp = stack->next;
 
-    stack->next = stack->next->prev;
+	if (stack->next->prev != NULL)
+		stack->next->prev->next = NULL;
 
-    free(temp);
-    stack->n--;
+	stack->next = stack->next->prev;
 
-    return value;
+	free(temp);
+	stack->n--;
+
+	return (value);
 }
 
-// Queue front operation
+/**
+ * queue_front - returns the value at the front of a stack_t list
+ *
+ * @stack: pointer to the stack
+ *
+ * Return: the value at the front of the stack
+*/
 int queue_front(stack_t *stack)
 {
-    return stack_front(stack);
-}
-
-// Queue back operation
-int queue_back(stack_t *stack)
-{
-    if (stack->n == 0)
-    {
-        printf("Queue is empty.\n");
-        return -1;
-    }
-
-    stack_t *temp = stack->next;
-
-    while (temp->prev != NULL)
-        temp = temp->prev;
-
-    return temp->n;
+	return (stack_front(stack));
 }
