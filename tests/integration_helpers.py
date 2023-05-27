451d8ab66ed9f222861e81b8e1776d5a720c18f4
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-05-28 01:13:58 +0300

    unit and integration tests support in build system

---
+++ tests/integration_helpers.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import difflib
+import re
+import inspect
+
+def run_test(file):
+    filename = os.path.basename(inspect.stack()[1].filename)
+
+    # run your compiled monty executable and capture its output
+    monty_process = subprocess.run(["./build/monty", file], capture_output=True, text=True)
+    monty_output = monty_process.stdout.strip()
+    monty_stderr = monty_process.stderr.strip()
+
+    # run correct monty implementation and capture its output
+    cmonty_process = subprocess.run(["./tests/monty", file], capture_output=True, text=True)
+    cmonty_output = cmonty_process.stdout.strip()
+    cmonty_stderr = cmonty_process.stderr.strip()
+
+    # compare the output of your program and correct implementation
+    out, err = difflib.SequenceMatcher(None, monty_output, cmonty_output), difflib.SequenceMatcher(None, monty_stderr, cmonty_stderr)
+    if out.ratio() == 1 and len(monty_stderr.split('\n')) == len(cmonty_stderr.split('\n')) and monty_process.returncode == cmonty_process.returncode:
+        print(f"integration test {filename} passed!")
+    else:
+        print(f"integration test {filename} failed.")
+        if len(monty_stderr.split('\n')) != len(cmonty_stderr.split('\n')): #err.ratio() != 1
+            print(f"stderr is different")
+            print("current error: \n", monty_stderr)
+            print("correct error: \n", cmonty_stderr)
+            dif = difflib.Differ()
+            monty_diff = dif.compare(monty_stderr.splitlines(), cmonty_stderr.splitlines())
+            print("\n".join(monty_diff))
+        if out.ratio() != 1:
+            print(f"stdout is different")
+            print("current output: \n", monty_output)
+            print("correct output: \n", cmonty_output)
+            dif = difflib.Differ()
+            monty_diff = dif.compare(monty_output.splitlines(), cmonty_output.splitlines())
+            print("\n".join(monty_diff))
+        if monty_process.returncode != cmonty_process.returncode:
+            print(f"exit status is different")
+            print(f"current exit code: {monty_process}")
+            print(f"correct exit code: {cmonty_process}")
---
+++ tests/test.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <stdio.h>
+#include "../stack.h"
+
+int line_number = 1;
+stack_t stack;
+
+#endif /* TEST_H */
